b4c752985df80d86736b745fa0dd40a1c5fbaa0a
Patrick Auernig <patrick.auernig@mykolab.com>
2025-01-19 17:35:13 +0100

    chore: update nix flake
    This bumps the locked flake versions, removes the dependency on flake-utils, and makes sure that
    rust-overlay uses the same nixpkgs as the root input instead of pulling its own copy.

--- flake.nix
+++ flake.nix
@@ -2,39 +2,47 @@
   description = "A basic Rust devshell for NixOS users developing Leptos";
 
   inputs = {
-    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url  = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay = {
+        url = "github:oxalica/rust-overlay";
+        inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
-      in
-      with pkgs;
-      {
-        devShells.default = mkShell {
-          buildInputs = [
-            openssl
-            pkg-config
-            cacert
-            cargo-make
-            trunk
-            (rust-bin.selectLatestNightlyWith( toolchain: toolchain.default.override {
-              extensions= [ "rust-src" "rust-analyzer" ];
-              targets = [ "wasm32-unknown-unknown" ];
-            }))
-          ] ++ pkgs.lib.optionals pkg.stdenv.isDarwin [
-            darwin.apple_sdk.frameworks.SystemConfiguration
-          ];
+  outputs = { nixpkgs, rust-overlay, ... }:
+  let
+    lib = nixpkgs.lib;
+    systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
 
-          shellHook = ''
-            '';
-        };
-      }
-    );
+    forEachSystem = fn: lib.genAttrs systems (system: let
+      overlays = [ (import rust-overlay) ];
+      pkgs = import nixpkgs { inherit system overlays; };
+    in fn { inherit pkgs; } );
+
+    mkRustToolchain = pkgs: pkgs.rust-bin.selectLatestNightlyWith (t: t.minimal.override {
+      extensions= [ "rust-src" "rust-analyzer" ];
+      targets = [ "wasm32-unknown-unknown" ];
+    });
+  in {
+    devShells = forEachSystem ({ pkgs }: {
+      default = pkgs.mkShell {
+        name = "leptos";
+
+        buildInputs = with pkgs; [
+          openssl
+          pkg-config
+          cacert
+          (mkRustToolchain pkgs)
+        ] ++ pkgs.lib.optionals pkg.stdenv.isDarwin [
+          darwin.apple_sdk.frameworks.SystemConfiguration
+        ];
+
+        # Tools
+        packages = with pkgs; [
+          cargo-make
+          trunk
+        ];
+      };
+    });
+  };
 }
